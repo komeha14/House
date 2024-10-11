d144455d985d148dadfb74ad80cf1d5b09ce5535
komeha14 <119334738+komeha14@users.noreply.github.com>
2024-10-11 23:27:43 +0300

    Update app.py

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify, render_template
+import pickle
+import numpy as np
+
+# تحميل النموذج المحفوظ
+model = pickle.load(open('best_house_price_model.pkl', 'rb'))
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # الحصول على البيانات المدخلة من المستخدم وتحويلها بالشكل المطلوب
+    carpet_area = int(request.form['carpet_area'])
+    bathroom = int(request.form['bathroom'])
+    balcony = int(request.form['balcony'])
+    furnishing = int(request.form['furnishing'])
+    floor = int(request.form['floor'])
+    facing = int(request.form['facing'])
+
+    # تحويل المدخلات إلى تنسيق numpy array
+    features = np.array([[carpet_area, bathroom, balcony, furnishing, floor, facing]])
+    
+    # عمل التنبؤ باستخدام النموذج
+    prediction = model.predict(features)
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text=f'Predicted House Price: {output} rupees')
+
+if __name__ == "__main__":
+    app.run(debug=True)
